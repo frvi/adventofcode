f128aa25c795fa0c4c801fa807805092dc96a947
frvi <fredrik.wihlborg@gmail.com>
2021-12-05 12:06:45 +0100

    Day five, further cleanup

--- 2021/src/test/java/com/githib/frvi/aoc/twentytwentyone/day05/Day05Test.java
+++ 2021/src/test/java/com/githib/frvi/aoc/twentytwentyone/day05/Day05Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.awt.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -98,66 +99,57 @@ public class Day05Test {
     }
 
     private List<Point> straightLines(List<List<Point>> points) {
-        final var straightLines = new ArrayList<Point>();
+        List<Point> horizontal = new ArrayList<>();
+        List<Point> vertical = new ArrayList<>();
         for (var pair : points) {
             final var start = pair.get(0);
             final var stop = pair.get(1);
 
-            if (start.x == stop.x) {
-                final var min = Math.min(start.y, stop.y);
-                final var max = Math.max(start.y, stop.y);
-                straightLines.addAll(IntStream.range(min, max + 1).mapToObj(y -> new Point(start.x, y)).toList());
+            final var x1 = start.x;
+            final var x2 = stop.x;
+            final var y1 = start.y;
+            final var y2 = stop.y;
+            final var dx = x2 - x1;
+            final var dy = y2 - y1;
+
+            if (dx == 0) {
+                var ys = dy > 0 ? createRange(y1, y2) : reverseRange(y1, y2);
+                vertical.addAll(ys.stream().map(y -> new Point(x1, y)).toList());
             }
-            if (start.y == stop.y) {
-                final var min = Math.min(start.x, stop.x);
-                final var max = Math.max(start.x, stop.x);
-                straightLines.addAll(IntStream.range(min, max + 1).mapToObj(x -> new Point(x, start.y)).toList());
+            if (dy == 0) {
+                var xs = dx > 0 ? createRange(x1, x2) : reverseRange(x1, x2);
+                horizontal.addAll(xs.stream().map(x -> new Point(x, y1)).toList());
             }
         }
-        return straightLines;
+        return Stream.of(horizontal, vertical)
+                .flatMap(Collection::stream)
+                .toList();
     }
 
     private List<Point> diagonalLines(List<List<Point>> pairs) {
-        final var diagonalLines = new ArrayList<Point>();
+        final var diagonal = new ArrayList<Point>();
         for (var pair : pairs) {
             final var start = pair.get(0);
             final var stop = pair.get(1);
+            final var x1 = start.x;
+            final var x2 = stop.x;
+            final var y1 = start.y;
+            final var y2 = stop.y;
+            final var dx = x2 - x1;
+            final var dy = y2 - y1;
 
-            if (start.x < stop.x) {
-                final var xs = createRange(start.x, stop.x);
-                if (start.y > stop.y) {
-                    final var ys = createRange(stop.y, start.y);
-                    Collections.reverse(ys);
-                    for (int i = 0; i < xs.size(); i++) {
-                        diagonalLines.add(new Point(xs.get(i), ys.get(i)));
-                    }
-                }
-                if (start.y < stop.y) {
-                    final var ys = createRange(start.y, stop.y);
-                    for (int i = 0; i < xs.size(); i++) {
-                        diagonalLines.add(new Point(xs.get(i), ys.get(i)));
-                    }
-                }
+            if (dx == 0 || dy == 0) {
+                continue;
             }
-            if (start.x > stop.x) {
-                final var xs = createRange(stop.x, start.x);
-                Collections.reverse(xs);
-                if (start.y > stop.y) {
-                    final var ys = createRange(stop.y, start.y);
-                    Collections.reverse(ys);
-                    for (int i = 0; i < xs.size(); i++) {
-                        diagonalLines.add(new Point(xs.get(i), ys.get(i)));
-                    }
-                }
-                if (start.y < stop.y) {
-                    final var ys = createRange(start.y, stop.y);
-                    for (int i = 0; i < xs.size(); i++) {
-                        diagonalLines.add(new Point(xs.get(i), ys.get(i)));
-                    }
-                }
+
+            var xs = dx > 0 ? createRange(x1, x2) : reverseRange(x1, x2);
+            var ys = dy > 0 ? createRange(y1, y2) : reverseRange(y1, y2);
+
+            for (int i = 0; i < xs.size(); i++) {
+                diagonal.add(new Point(xs.get(i), ys.get(i)));
             }
         }
-        return diagonalLines;
+        return diagonal;
     }
 
     private long overlaps(List<Point> points) {
@@ -170,9 +162,11 @@ public class Day05Test {
         return map.values().stream().filter(v -> v >= 2).count();
     }
 
-    private ArrayList<Integer> createRange(int startInclusive, int stopInclusive) {
-        return IntStream.range(startInclusive, stopInclusive + 1)
-                .boxed()
-                .collect(Collectors.toCollection(ArrayList::new));
+    private List<Integer> createRange(int startInclusive, int stopInclusive) {
+        return IntStream.range(startInclusive, stopInclusive + 1).boxed().toList();
+    }
+
+    private List<Integer> reverseRange(int startInclusive, int stopInclusive) {
+        return createRange(stopInclusive, startInclusive).stream().sorted(Collections.reverseOrder()).toList();
     }
 }
