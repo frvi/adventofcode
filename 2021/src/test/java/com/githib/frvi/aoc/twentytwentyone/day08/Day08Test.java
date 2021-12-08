29f09f849db56e590943efc009a3e27bea890d08
frvi <fredrik.wihlborg@gmail.com>
2021-12-08 09:02:06 +0100

    Day eigth, prepare

---
+++ 2021/src/test/java/com/githib/frvi/aoc/twentytwentyone/day08/Day08Test.java
@@ -0,0 +1,20 @@
+package com.githib.frvi.aoc.twentytwentyone.day08;
+
+import com.githib.frvi.aoc.twentytwentyone.helper.Helper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Day08Test {
+
+    public static final String SAMPLE = "src/test/resources/input.08.sample";
+    public static final String INPUT = "src/test/resources/input.08";
+
+    @Test
+    public void sampleOne() throws IOException {
+        // given
+        final var input = Helper.readAllLines(SAMPLE);
+        final var expected = 26;
+
+    }
+}
